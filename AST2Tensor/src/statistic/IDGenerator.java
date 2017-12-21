8cb5d12978d2f4d841aaa9a69ec4b2432274355d
DELL <DELL@DESKTOP-20DEPKI>
2017-12-21 23:37:42 +0800

    small change.

--- AST2Tensor/src/statistic/IDGenerator.java
+++ AST2Tensor/src/statistic/IDGenerator.java
@@ -1,20 +1,16 @@
 package statistic;
 
+import java.util.List;
+
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.CharacterLiteral;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.NullLiteral;
-import org.eclipse.jdt.core.dom.NumberLiteral;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.StringLiteral;
 
-import logger.DebugLogger;
+import eclipse.search.JDTSearchForChildrenOfASTNode;
 import statistic.id.IDCounter;
 import statistic.id.IDManager;
-import util.SystemUtil;
 
 public class IDGenerator extends ASTVisitor {
 
@@ -31,68 +27,72 @@ public class IDGenerator extends ASTVisitor {
 	}
 
 	@Override
-	public boolean preVisit2(ASTNode node) {
-		if (!IsLeafNode(node)) {
+	public void preVisit(ASTNode node) {
+		super.preVisit(node);
+		List<ASTNode> children = JDTSearchForChildrenOfASTNode.GetChildren(node);
+		if (children.size() > 0) {
 			ic.EncounterANode(IDManager.PrimordialNonLeafASTType, node.getClass().getSimpleName());
-		}
-		return super.preVisit2(node);
-	}
-
-	private boolean IsLeafNode(ASTNode node) {
-		if (node instanceof SimpleName || node instanceof NumberLiteral || node instanceof CharacterLiteral
-				|| node instanceof NullLiteral || node instanceof StringLiteral) {
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public boolean visit(SimpleName node) {
-		if (node.getParent() != null) {
-			ASTNode parent = node.getParent();
-			String type = parent.getClass().getSimpleName();
-			String content = node.getIdentifier();
-			ic.EncounterANode(type, content);
 		} else {
-			DebugLogger.Error("this should not happen: SimpleName has no parent ASTNode!!!");
-			SystemUtil.Delay(10000);
-			String type = node.getClass().getSimpleName();
-			String content = node.getIdentifier();
-			ic.EncounterANode(type, content);
+			String type = node.getParent().getClass().getSimpleName() + "#" + node.getClass().getSimpleName();
+			ic.EncounterANode(type, node.toString());
 		}
-		return super.visit(node);
 	}
-
-	@Override
-	public boolean visit(NumberLiteral node) {
-		String type = node.getClass().getSimpleName();
-		String content = node.getToken();
-		ic.EncounterANode(type, content);
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(CharacterLiteral node) {
-		String type = node.getClass().getSimpleName();
-		String content = node.getEscapedValue();
-		ic.EncounterANode(type, content);
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(StringLiteral node) {
-		String type = node.getClass().getSimpleName();
-		String content = node.getEscapedValue();
-		ic.EncounterANode(type, content);
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(NullLiteral node) {
-		String type = node.getClass().getSimpleName();
-		String content = node.toString();
-		ic.EncounterANode(type, content);
-		return super.visit(node);
-	}
+	
+//	private boolean IsLeafNode(ASTNode node) {
+//		if (node instanceof SimpleName || node instanceof NumberLiteral || node instanceof CharacterLiteral
+//				|| node instanceof NullLiteral || node instanceof StringLiteral) {
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	@Override
+//	public boolean visit(SimpleName node) {
+//		if (node.getParent() != null) {
+//			ASTNode parent = node.getParent();
+//			String type = parent.getClass().getSimpleName();
+//			String content = node.getIdentifier();
+//			ic.EncounterANode(type, content);
+//		} else {
+//			DebugLogger.Error("this should not happen: SimpleName has no parent ASTNode!!!");
+//			SystemUtil.Delay(10000);
+//			String type = node.getClass().getSimpleName();
+//			String content = node.getIdentifier();
+//			ic.EncounterANode(type, content);
+//		}
+//		return super.visit(node);
+//	}
+//
+//	@Override
+//	public boolean visit(NumberLiteral node) {
+//		String type = node.getClass().getSimpleName();
+//		String content = node.getToken();
+//		ic.EncounterANode(type, content);
+//		return super.visit(node);
+//	}
+//
+//	@Override
+//	public boolean visit(CharacterLiteral node) {
+//		String type = node.getClass().getSimpleName();
+//		String content = node.getEscapedValue();
+//		ic.EncounterANode(type, content);
+//		return super.visit(node);
+//	}
+//
+//	@Override
+//	public boolean visit(StringLiteral node) {
+//		String type = node.getClass().getSimpleName();
+//		String content = node.getEscapedValue();
+//		ic.EncounterANode(type, content);
+//		return super.visit(node);
+//	}
+//
+//	@Override
+//	public boolean visit(NullLiteral node) {
+//		String type = node.getClass().getSimpleName();
+//		String content = node.toString();
+//		ic.EncounterANode(type, content);
+//		return super.visit(node);
+//	}
 
 }
