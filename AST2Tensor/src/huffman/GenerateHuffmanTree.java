ebd5d5c350917b85d881a08d89ef53f935c2b17d
yixiao yang <yangyixiaofirst@163.com>
2017-12-29 12:10:49 +0800

    small change.

--- AST2Tensor/src/huffman/GenerateHuffmanTree.java
+++ AST2Tensor/src/huffman/GenerateHuffmanTree.java
@@ -1,36 +1,41 @@
 package huffman;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.Stack;
 
 public class GenerateHuffmanTree {
-	
-	public static HuffmanNode buildTree(Map<String, Integer> statistics) {
+
+	public static HuffmanNode BuildTree(Map<Integer, Integer> statistics) {
 		// , List<HuffmanNode> leafs
-		Set<String> keys = statistics.keySet();
+		Set<Integer> keys = statistics.keySet();
 		// Character[] keys = .toArray(new Character[0]);
 
 		PriorityQueue<HuffmanNode> priorityQueue = new PriorityQueue<HuffmanNode>();
-		for (String key : keys) {
+		for (Integer key : keys) {
 			HuffmanNode node = new HuffmanNode();
 			node.setContent(key);
 			node.setFrequence(statistics.get(key));
+			node.setLeafNodeNum(1);
 			priorityQueue.add(node);
 		}
 
 		int size = priorityQueue.size();
-		for (int i = 1; i <= size - 1; i++) {
+		for (int i = 1; i < size - 1; i++) {
 			HuffmanNode node1 = priorityQueue.poll();
 			HuffmanNode node2 = priorityQueue.poll();
-			
+
 			HuffmanNode sumNode = new HuffmanNode();
 			sumNode.setContent(null);// node1.getContent() + node2.getContent()
 			sumNode.setFrequence(node1.getFrequence() + node2.getFrequence());
-			
+			sumNode.setMaxDepth(Math.max(node1.getMaxDepth(), node2.getMaxDepth()) + 1);
 			sumNode.setLeftNode(node1);
 			sumNode.setRightNode(node2);
-			
+			sumNode.setLeafNodeNum(node1.getLeafNodeNum() + node2.getLeafNodeNum());
+			sumNode.setTotalNodeNum(sumNode.getTotalNodeNum() + node1.getTotalNodeNum() + node2.getTotalNodeNum());
+
 			node1.setParent(sumNode);
 			node2.setParent(sumNode);
 
@@ -39,5 +44,33 @@ public class GenerateHuffmanTree {
 
 		return priorityQueue.poll();
 	}
-	
+
+	public static int[][] BuildEncodeTensor(HuffmanNode root) {
+		int width = root.getMaxDepth();
+		int height = root.getLeafNodeNum();
+		int[][] tensor = new int[height][width];
+		Arrays.fill(tensor, -1);
+		RecursiveBuildEncodeTensor(root, tensor, new Stack<Integer>());
+		return tensor;
+	}
+
+	private static void RecursiveBuildEncodeTensor(HuffmanNode root, int[][] tensor, Stack<Integer> path) {
+		if (root.isLeaf()) {
+			Integer[] path_arr = new Integer[path.size()];
+			path.toArray(path_arr);
+			System.arraycopy(tensor[root.getContent()], 0, path_arr, 0, path_arr.length);
+		} else {
+			if (root.hasLeftChild()) {
+				path.push(1);
+				RecursiveBuildEncodeTensor(root.getLeftNode(), tensor, path);
+				path.pop();
+			}
+			if (root.hasRightChild()) {
+				path.push(0);
+				RecursiveBuildEncodeTensor(root.getRightNode(), tensor, path);
+				path.pop();
+			}
+		}
+	}
+
 }
--- AST2Tensor/src/huffman/HuffmanNode.java
+++ AST2Tensor/src/huffman/HuffmanNode.java
@@ -1,13 +1,24 @@
 package huffman;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HuffmanNode implements Comparable<HuffmanNode> {
-	private String chars = null;
+	private Integer content = null;
 	private int frequence = 0;
 	private HuffmanNode parent = null;
 	private HuffmanNode leftNode = null;
 	private HuffmanNode rightNode = null;
+	private int maxDepth = 0;
+	private int leafNodeNum = 0;
+	private int totalNodeNum = 1;
+	
+	public void setTotalNodeNum(int totalNodeNum) {
+		this.totalNodeNum = totalNodeNum;
+	}
+	
+	public int getTotalNodeNum() {
+		return totalNodeNum;
+	}
 
 	@Override
 	public int compareTo(HuffmanNode n) {
@@ -15,7 +26,7 @@ public class HuffmanNode implements Comparable<HuffmanNode> {
 	}
 
 	public boolean isLeaf() {
-		return chars.length() == 1;
+		return leftNode == null && rightNode == null;
 	}
 
 	public boolean isRoot() {
@@ -24,6 +35,14 @@ public class HuffmanNode implements Comparable<HuffmanNode> {
 
 	public boolean isLeftChild() {
 		return parent != null && this == parent.leftNode;
+	}
+	
+	public boolean hasLeftChild() {
+		return leftNode != null;
+	}
+	
+	public boolean hasRightChild() {
+		return rightNode != null;
 	}
 
 	public int getFrequence() {
@@ -34,12 +53,12 @@ public class HuffmanNode implements Comparable<HuffmanNode> {
 		this.frequence = frequence;
 	}
 
-	public String getContent() {
-		return chars;
+	public Integer getContent() {
+		return content;
 	}
 
-	public void setContent(String chars) {
-		this.chars = chars;
+	public void setContent(Integer content) {
+		this.content = content;
 	}
 
 	public HuffmanNode getParent() {
@@ -66,8 +85,53 @@ public class HuffmanNode implements Comparable<HuffmanNode> {
 		this.rightNode = rightNode;
 	}
 	
-	public void ToTensor(ArrayList<Integer> node, ArrayList<Integer> left_child, ArrayList<Integer> right_child) {
-		node.add();
+	public int getMaxDepth() {
+		return maxDepth;
+	}
+
+	public void setMaxDepth(int maxDepth) {
+		this.maxDepth = maxDepth;
+	}
+	
+	public int getLeafNodeNum() {
+		return leafNodeNum;
+	}
+
+	public void setLeafNodeNum(int leafNodeNum) {
+		this.leafNodeNum = leafNodeNum;
+	}
+	
+	// ArrayList<Integer> node, ArrayList<Integer> left_child, ArrayList<Integer> right_child
+	public int[][] ToTensor() {
+		int[][] tensor = new int[3][totalNodeNum];
+		Arrays.fill(tensor, -1);
+		IDAssigner ida = new IDAssigner();
+		ToTensor(tensor, ida);
+		return tensor;
+	}
+	
+	private int ToTensor(int[][] tensor, IDAssigner ida) {
+		int id = ida.GetNewID();
+		int infix_right_id = rightNode.ToTensor(tensor, ida);
+		int infix_left_id = leftNode.ToTensor(tensor, ida);
+		tensor[0][id] = infix_right_id;
+		tensor[1][id] = infix_left_id;
+		tensor[2][id] = (content == null ? -1 : content);
+		return id;
+	}
+	
+}
+
+class IDAssigner {
+	
+	int id = -1;
+	
+	public IDAssigner() {
+	}
+	
+	public int GetNewID() {
+		id++;
+		return id;
 	}
 	
 }
