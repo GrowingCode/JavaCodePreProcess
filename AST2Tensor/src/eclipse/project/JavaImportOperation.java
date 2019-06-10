4b3f4305c43c5106ce8e40ffc64b541ee80a5453
yangy <yangy@192.168.1.104>
2019-06-11 00:54:33 +0800

    small change.

--- AST2Tensor/src/eclipse/project/JavaImportOperation.java
+++ AST2Tensor/src/eclipse/project/JavaImportOperation.java
@@ -15,39 +15,39 @@ import org.eclipse.ui.wizards.datatransfer.FileSystemStructureProvider;
 import org.eclipse.ui.wizards.datatransfer.ImportOperation;
 
 public class JavaImportOperation {
-	
-	// Set<String> 
-	public static void ImportFileSystem(IJavaProject javaProject, Map<String, TreeMap<String, String>> dir_files_map) throws JavaModelException {
+
+	// Set<String>
+	public static void ImportFileSystem(IJavaProject javaProject, Map<String, TreeMap<String, String>> dir_files_map)
+			throws JavaModelException {
 		// Set<String> analysis_classes = new HashSet<String>();
-		
+
 		IOverwriteQuery overwriteQuery = new IOverwriteQuery() {
 			public String queryOverwrite(String file) {
 				return ALL;
 			}
 		};
-		
+
 		IPath src = JavaProjectElementFinder.FindSourceFolder(javaProject);
-		
+
 		int index = 0;
 		ArrayList<File> filesToImport = new ArrayList<File>();
 		Set<String> dirs = dir_files_map.keySet();
 		Iterator<String> diritr = dirs.iterator();
-		while (diritr.hasNext())
-		{
+		while (diritr.hasNext()) {
 			index++;
 			String dir = diritr.next();
 			TreeMap<String, String> files = dir_files_map.get(dir);
 			Set<String> fkeys = files.keySet();
 			Iterator<String> fitr = fkeys.iterator();
-			while (fitr.hasNext())
-			{
+			while (fitr.hasNext()) {
 				String fkey = fitr.next();
 				String fvalue = files.get(fkey);
 				// analysis_classes.add(fkey);
 				filesToImport.add(new File(fvalue));
 			}
 
-			System.out.println("==== Import " + index + "/" + dir_files_map.size() + "#" + dir + ": " + files.size() + " files ====");
+			System.out.println("==== Import " + index + "/" + dir_files_map.size() + "#" + dir + ": " + files.size()
+					+ " files ====");
 			WaitOverMonitor wom = new WaitOverMonitor();
 			ImportOperation importOperation = new ImportOperation(src, new File(dir),
 					FileSystemStructureProvider.INSTANCE, overwriteQuery, filesToImport);
@@ -58,9 +58,9 @@ public class JavaImportOperation {
 				e.printStackTrace();
 			}
 			wom.WaitToStop();
-//			System.out.println("==== End import " + dir_files_map.size() + " files ====");
+			System.out.println("==== End import " + dir_files_map.size() + " files ====");
 		}
 		// return analysis_classes;
 	}
-	
+
 }
