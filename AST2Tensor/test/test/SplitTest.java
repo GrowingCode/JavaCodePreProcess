f87b653ce9e1328d9ad41b19fb77f66af3224b0e
yangy <yangy@192.168.1.104>
2019-09-06 22:15:53 +0800

    small change.

--- AST2Tensor/test/test/SplitTest.java
+++ AST2Tensor/test/test/SplitTest.java
@@ -7,7 +7,7 @@ import statistic.YTokenizer;
 public class SplitTest {
 	
 	public static void main(String[] args) {
-		ArrayList<String> ll = YTokenizer.GetTokens("if (a && b) {i = i+1;}");
+		ArrayList<String> ll = YTokenizer.GetTokens("if (a && b) {i = i+1;System.out.println(\"ha\\\"ha\")}");
 		for (String str_l : ll) {
 			System.out.println(str_l);
 		}
