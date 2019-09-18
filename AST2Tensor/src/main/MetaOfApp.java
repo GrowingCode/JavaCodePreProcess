43f11fcd97c8da3dca0c4f8309614a2542cf9240
yangy <yangy@192.168.1.104>
2019-09-18 09:48:04 +0800

    small change.

--- AST2Tensor/src/main/MetaOfApp.java
+++ AST2Tensor/src/main/MetaOfApp.java
@@ -45,7 +45,7 @@ public class MetaOfApp {
 	
 	public final static int number_of_merges = 1000;
 	
-	public final static int MinimumNumberOfStatementsInAST = 10;
+//	public final static int MinimumNumberOfStatementsInAST = 10;
 	public final static int MinimumNumberOfNodesInAST = 100;
 	
 	public static boolean StatementNoLimit = false;
