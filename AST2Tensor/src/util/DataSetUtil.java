54d6dafcb8f0d7cd657b8c4e61c071ebf444a031
yangy <yangyixiaofirst@163.com>
2019-11-17 23:49:26 +0800

    small change.

--- AST2Tensor/src/util/DataSetUtil.java
+++ AST2Tensor/src/util/DataSetUtil.java
@@ -3,17 +3,18 @@ package util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.eclipse.core.runtime.Assert;
-
 public class DataSetUtil {
 
 	public static String FilterNumberPrefix(String with_number_prefix) {
 		String reg = "^(\\d+_)";
 		Pattern p2 = Pattern.compile(reg);
 		Matcher m2 = p2.matcher(with_number_prefix);
-		Assert.isTrue(m2.find());
+		if (m2.find()) {
+			return with_number_prefix.substring(m2.group(1).length());
+		} else {
+			return with_number_prefix;
+		}
 //		System.out.println(m2.group(1));
-		return with_number_prefix.substring(m2.group(1).length());
 	}
 	
 	public static void main(String[] args) {
