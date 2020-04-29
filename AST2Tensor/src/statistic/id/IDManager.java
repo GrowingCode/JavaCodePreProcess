831494fa1254d29be599f73059ea3762f5ebb94c
yangy <yangy@DESKTOP-SK2FI36>
2020-04-29 20:25:00 +0800

    small change.

--- AST2Tensor/src/statistic/id/IDManager.java
+++ AST2Tensor/src/statistic/id/IDManager.java
@@ -354,7 +354,7 @@ public class IDManager {
 	public int GetSkeletonID(String skeleton) {
 		Integer id = skeleton_id_map.get(skeleton);
 		Assert.isTrue(id != null, "unseen skeleton:" + skeleton);
-		if (MetaOfApp.TakeUnseenAsUnk && id < skeleton_hit_num) {
+		if (MetaOfApp.OutOfScopeReplacedByUnk && id >= skeleton_hit_num) {
 			return skeleton_id_map.get(Unk);
 		}
 //		if (id == null) {
@@ -373,7 +373,8 @@ public class IDManager {
 	public int GetTypeContentID(String type_content) {
 		Integer id = token_id_map.get(type_content);
 		Assert.isTrue(id != null, "unseen type_content:" + type_content);
-		if (MetaOfApp.TakeUnseenAsUnk && id < token_hit_num) {
+		if (MetaOfApp.OutOfScopeReplacedByUnk && id >= token_hit_num) {
+//			System.err.println("id:"+id+"#token_hit_num:"+token_hit_num);
 			return token_id_map.get(Unk);
 		}
 //		if (id == null) {
