ce0fb607033e7028ffaa4ae61eca9dc0ec0e2c04
yangy <yangy@DESKTOP-SK2FI36>
2020-05-02 14:58:14 +0800

    small change.

--- AST2Tensor/src/translation/tensor/util/RepetitionUtil.java
+++ AST2Tensor/src/translation/tensor/util/RepetitionUtil.java
@@ -16,19 +16,23 @@ public class RepetitionUtil {
 		Map<Integer, Integer> latest_index = new TreeMap<Integer, Integer>();
 		int i_len = stmt_token_variable_info.size();
 		ArrayList<Integer> seq_var_info = new ArrayList<Integer>();
+		ArrayList<Integer> real_var_info = new ArrayList<Integer>();
 		for (int i = 0; i < i_len; i++) {
 			Assert.isTrue(seq_var_info.size() == i);
 			Integer ti = stmt_token_variable_info.get(i);
 			if (ti >= 0) {
 				Integer li = latest_index.get(ti);
+				int curr_var_index = real_var_info.size();
 				if (li != null) {
-					int relative = latest_index.size() - li;
+					int relative = curr_var_index - li;
+					Assert.isTrue(relative > 0);
 					seq_var_info.add(relative);// - i
 //					System.out.println("token_i:" + i + "token_en:" + ti + "#relative:" + relative);
 				} else {
 					seq_var_info.add(-1);// Integer.MAX_VALUE
 				}
-				latest_index.put(ti, latest_index.size());
+				real_var_info.add(ti);
+				latest_index.put(ti, curr_var_index);
 			} else {
 				seq_var_info.add(-1);
 			}
