c393b62eef589cebbc71ecddac9173b59c051c8c
yangy <yangy@DESKTOP-2LH9TM6>
2020-07-31 22:21:41 +0800

    small change.

--- AST2Tensor/src/bpe/skt/SktForestGenerator.java
+++ AST2Tensor/src/bpe/skt/SktForestGenerator.java
@@ -113,7 +113,7 @@ class SktTreeGenerator extends ASTVisitor {
 		boolean to_create_tree_node = true;
 		String node_whole_cnt = node.toString();
 		if (node instanceof Statement) {
-			if (t_root != node) {
+			if ((node instanceof Block) || (t_root != node)) {
 				ctn = ctn && false;
 				ctn_handle = ctn_handle && false;
 			}
