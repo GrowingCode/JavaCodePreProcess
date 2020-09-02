ce79e32f27617ae92553f64a11fb5941024766e7
yangy <yangy@DESKTOP-GKJ2OL2>
2020-09-02 10:00:32 +0800

    small change.

--- AST2Tensor/src/tree/TreeFlatten.java
+++ AST2Tensor/src/tree/TreeFlatten.java
@@ -19,6 +19,9 @@ public class TreeFlatten {
 	public ArrayList<ArrayList<String>> skt_pe_struct_v_tree_uid = new ArrayList<ArrayList<String>>();
 	
 	public ArrayList<String> skt_e_struct = new ArrayList<String>();
+	public ArrayList<String> skt_e_struct_h_count = new ArrayList<String>();
+	public ArrayList<String> skt_e_struct_v_count = new ArrayList<String>();
+	
 	public ArrayList<String> skt_token = new ArrayList<String>();
 	public ArrayList<Integer> skt_token_is_var = new ArrayList<Integer>();
 	public ArrayList<Integer> skt_token_kind = new ArrayList<Integer>();
