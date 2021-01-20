9ae35c59fc67c3d29a7e95ce5b1ca3817a6f1f1e
yangy <yangy@DESKTOP-GKJ2OL2>
2021-01-20 13:01:16 +0800

    small change.

--- AST2Tensor/src/main/MetaOfApp.java
+++ AST2Tensor/src/main/MetaOfApp.java
@@ -89,12 +89,12 @@ public class MetaOfApp {
 	public final static int MaximumTokenCapacity = 50000;
 	public final static int MaximumSkeletonTokenCapacity = 25000;
 	
-	public final static int MinimumNotUnkAppearTime = 3;
+	public final static int MinimumNotUnkAppearTime = 2;
 	public final static int MinimumSkeletonNotUnkAppearTime = 2;
 	
 	public static boolean OutOfScopeReplacedByUnk = true;
 	public final static int NumberOfSkeletonUnk = 5;
-	public final static int MinimumNumberOfSkeletonVocabulary = 20;
+	public final static int MinimumNumberOfSkeletonVocabulary = 50;
 	public final static int NumberOfUnk = 5;
 	public final static int MinimumNumberOfVocabulary = 50;
 	public final static int MaxParentTypeRemoveTimes = 1;
