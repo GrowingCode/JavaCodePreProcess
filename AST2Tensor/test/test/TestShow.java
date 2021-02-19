c9f7116cfb6e860714847080ce417f0f66af3683
yangy <yangy@DESKTOP-PV86UN0>
2021-02-19 20:16:05 +0800

    small change.

---
+++ AST2Tensor/test/test/TestShow.java
@@ -0,0 +1,20 @@
+package test;
+
+public class TestShow {
+	
+	public int test(int a) {
+		return a+1;
+	}
+	
+	public int test2(int a, int b) {
+		return a+b;
+	}
+	
+	public int test3(int a, int b) {
+		if (a > 1) {
+			return b-5;
+		}
+		return a+b;
+	}
+	
+}
