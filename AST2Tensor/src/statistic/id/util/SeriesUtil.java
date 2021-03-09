ea0599a29902e621db6ceb51696e9cc416992b9f
yangy <yangy@DESKTOP-PV86UN0>
2021-03-09 20:58:58 +0800

    small change.

--- AST2Tensor/src/statistic/id/util/SeriesUtil.java
+++ AST2Tensor/src/statistic/id/util/SeriesUtil.java
@@ -24,5 +24,17 @@ public class SeriesUtil {
 		FileUtil.WriteToFile(new File(dir + "/" + "All_" + desc + "_id.json"), gson.toJson(ati_out));// type_id_json.toString()
 	}
 	
+	public static void GenerateOriMapJson(String dir, TreeMap<String, Integer> to_gen, String desc) {
+//		Map<Object, Object> ati_objs = new HashMap<Object, Object>();
+//		Map<Integer, String> ati_out = MapUtil.ReverseKeyValueInMap(to_gen);
+//		Set<Object> ati_keys = ati_out.keySet();
+//		Iterator<Object> ati_key_itr = ati_keys.iterator();
+//		while (ati_key_itr.hasNext()) {
+//			Object key = ati_key_itr.next();
+//			ati_objs.put(key, ati_out.get(key));
+//		}
+		Gson gson = new Gson();
+		FileUtil.WriteToFile(new File(dir + "/" + "All_" + desc + "_str_id.json"), gson.toJson(to_gen));// type_id_json.toString()
+	}
 	
 }
